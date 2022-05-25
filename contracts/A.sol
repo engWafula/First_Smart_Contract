170eb0c5d99efa92110d0a9c0807c41b9be6ab2b
engWafula <wafulaallan5@gmail.com>
2022-05-25 12:57:08 +0300

    finishing up the fundamentals

--- contracts/A.sol
+++ contracts/A.sol
@@ -1,45 +1,54 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity^0.8.0;
 import "hardhat/console.sol";
 
+import  "./Storage.sol";
+
 
 contract A{
+
+    AppStorage s;
+
     uint a;
     function setA(uint _a) public{
-        a=_a;
+        s.a=_a;
     }
 
     function getA() public view returns (uint){
-        return a;
+        return s.a;
     }
 }
 
 
 contract B{
-    uint b;
-    uint8 c;
-    uint8 d;
-    address contractA;
+    // uint b;
+    // uint8 c;
+    // uint8 d;
+    // address contractA;
+
+    AppStorage s;
 
     constructor(address _A){
-        contractA=_A;
-        b=14;
-        c=0x45;
-        d=0xF5;
+        s.contractA=_A;
+        s.b=14;
+        s.c=0x45;
+    
     }
 
 
     function setB(uint _b) public{
-        b=_b;
+        s.b=_b;
         // A(contractA).setA(_a+1);
 
         //Here we use a delegate call to implement the same functionality
 
-      (bool success, bytes memory bb) =contractA.delegatecall(
+      (bool success, bytes memory bb) =s.contractA.delegatecall(
     abi.encodeWithSignature("getA(uint256)",_b+1)
 );
     }
 
     function getB() public view returns (uint){
-        return b;
+        return s.b;
     }
 }
